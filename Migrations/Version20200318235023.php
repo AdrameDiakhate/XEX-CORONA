511f1d94687f5910a492c20be6e727a603a79d63
AdrameDiakhate <56621259+AdrameDiakhate@users.noreply.github.com>
2020-03-19 17:54:12 +0100

    Add files via upload

---
+++ Migrations/Version20200318235023.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200318235023 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE cas (id INT AUTO_INCREMENT NOT NULL, lieu_id INT NOT NULL, new_case_today INT NOT NULL, total_cases INT NOT NULL, new_death_today INT NOT NULL, total_deaths INT NOT NULL, INDEX IDX_3AD60B6AB213CC (lieu_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE faq (id INT AUTO_INCREMENT NOT NULL, question LONGTEXT NOT NULL, reponse LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE lieu (id INT AUTO_INCREMENT NOT NULL, region VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sensibilisation (id INT AUTO_INCREMENT NOT NULL, video VARCHAR(255) DEFAULT NULL, photo VARCHAR(255) DEFAULT NULL, texte LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cas ADD CONSTRAINT FK_3AD60B6AB213CC FOREIGN KEY (lieu_id) REFERENCES lieu (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cas DROP FOREIGN KEY FK_3AD60B6AB213CC');
+        $this->addSql('DROP TABLE cas');
+        $this->addSql('DROP TABLE faq');
+        $this->addSql('DROP TABLE lieu');
+        $this->addSql('DROP TABLE sensibilisation');
+    }
+}
